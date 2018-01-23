39d8d8b01ca095f896fbfda1241c35710ae17a5d
Niels Holt <niels@steenbuchholt.dk>
2018-01-23 17:26:01 +0100

    Using fcoo/grunt-fcoo-grunt-plugin^3.0.0 Updating demo with fcoo/jquery-base-slider^4.3.1

--- dist/jquery-time-slider.js
+++ dist/jquery-time-slider.js
@@ -74,7 +74,7 @@ options:
         };
 
     window.TimeSlider = function (input, options, pluginCount) {
-        this.VERSION = "5.0.0";
+        this.VERSION = "5.0.1";
 
         //Setting default options
         this.options = $.extend( true, {}, defaultOptions, options );
