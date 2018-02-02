f831c1b9e317995d3c2a595405bfd9d3f40c60af
Niels Holt <niels@steenbuchholt.dk>
2018-02-02 20:45:55 +0100

    Update demo

--- dist/jquery-time-slider.js
+++ dist/jquery-time-slider.js
@@ -76,7 +76,7 @@ options:
     window.TimeSlider = function (input, options, pluginCount) {
         var _this = this;
 
-        this.VERSION = "5.0.3";
+        this.VERSION = "5.0.4";
 
         //Setting default options
         this.options = $.extend( true, {}, defaultOptions, options );
